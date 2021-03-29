2cab18c48c5d495c1b7e4f683c81caf777a16b04
Khazdev <xazanoff17@gmail.com>
2021-03-29 19:31:16 +0300

    config fix

--- src/main/java/ru/khazanov/spring/config/SpringConfig.java
+++ src/main/java/ru/khazanov/spring/config/SpringConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -17,6 +18,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
  **/
 @Configuration
 @ComponentScan("ru.khazanov.spring")
+@EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
