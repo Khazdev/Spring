66debc0ed45b054f578c3e5325464f682a732fea
Khazdev <xazanoff17@gmail.com>
2021-03-29 19:24:25 +0300

    dispatcher mapping fix

--- src/main/java/ru/khazanov/spring/config/SpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/ru/khazanov/spring/config/SpringMvcDispatcherSerlvetIntitializer.java
@@ -18,6 +18,6 @@ public class SpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationCo
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
